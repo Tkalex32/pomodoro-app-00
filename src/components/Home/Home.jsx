14184e06f7fa254189611338c26e09562624f648
Tkalex32 <tkalexsc@gmail.com>
2022-02-28 09:14:55 +0100

    feat: add base design of Home and Settings  pages

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { buildStyles, CircularProgressbar } from "react-circular-progressbar";
+import { FaPlayCircle, FaPauseCircle, FaStopCircle } from "react-icons/fa";
+import "react-circular-progressbar/dist/styles.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <main>
+      <div className="title">Pomodoro Timer</div>
+      <div className="progress-bar">
+        <CircularProgressbar
+          value={60}
+          text={`05:20`}
+          strokeWidth={4}
+          styles={buildStyles({
+            pathColor: "#f54e4e",
+            trailColor: "#c5c5c51a",
+          })}
+        />
+      </div>
+      <div className="buttons">
+        <FaPlayCircle className="button" />
+        <div className="gap"></div>
+        <FaPauseCircle className="button" />
+      </div>
+    </main>
+  );
 };
 
 export default Home;
--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import ReactSlider from "react-slider";
 
 const Settings = () => {
-  return <div>Settings</div>;
+  return (
+    <main>
+      <div className="title">Settings</div>
+      <div className="slider-wrapper">
+        <label htmlFor="">Work:</label>
+        <ReactSlider
+          className="slider"
+          thumbClassName="thumb"
+          trackClassName="track"
+          value={25}
+          max={45}
+          min={1}
+        />
+
+        <label htmlFor="">Short break:</label>
+        <ReactSlider
+          className="slider short"
+          thumbClassName="thumb short"
+          trackClassName="track"
+          value={5}
+          max={20}
+          min={1}
+        />
+
+        <label htmlFor="">Long break:</label>
+        <ReactSlider
+          className="slider long"
+          thumbClassName="thumb long"
+          trackClassName="track"
+          value={20}
+          max={30}
+          min={1}
+        />
+      </div>
+      <button className="save-button">Save</button>
+    </main>
+  );
 };
 
 export default Settings;
